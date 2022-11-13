3d2022d20ef42d329135e52f6784a7f72665d552
Berkay-Disli <berkaydisli.work@gmail.com>
2022-11-13 22:19:09 +0300

    Initial Commit

---
+++ RiveComponents/RiveComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiveComponentsApp.swift
+//  RiveComponents
+//
+//  Created by Berkay Disli on 13.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct RiveComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
