328561fa33bdb1572a342abc327693bd7f0fbd56
Berkay-Disli <berkaydisli.work@gmail.com>
2022-11-14 16:04:29 +0300

    Page transition

--- RiveComponents/Onboarding.swift
+++ RiveComponents/Onboarding.swift
@@ -16,6 +16,7 @@ struct Onboarding: View {
     @State private var signInSuccessful: Bool?
     @State private var isLoading = false
     @FocusState private var focusedField: Field?
+    @State private var showOnboarding = true
     
     enum Field: Hashable {
             case usernameField
@@ -28,141 +29,164 @@ struct Onboarding: View {
                 .onTapGesture {
                     focusedField = .none
                 }
-            VStack(spacing: 0) {
-                loginCharacter.view()
-                    .frame(height: 325)
-                    .padding(.top, -60)
-                    .onTapGesture {
-                        focusedField = .none
-                    }
-                
-                ZStack {
-                    RoundedRectangle(cornerRadius: 20, style: .continuous)
-                        .fill(.white)
-                        .shadow(color: .gray.opacity(0.2), radius: 10, x: 0, y: 5)
+            if showOnboarding {
+                VStack(spacing: 0) {
+                    loginCharacter.view()
+                        .frame(height: 325)
+                        .padding(.top, -60)
                         .onTapGesture {
                             focusedField = .none
                         }
                     
-                    VStack(spacing: 0) {
-                        Text("TEDDY BEAR")
-                            .foregroundColor(Color("fg"))
-                            .font(.system(size: 40))
-                            .fontWeight(.medium)
-                            .tracking(10)
-                            .padding(.top)
-                            .offset(x: 5)
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20, style: .continuous)
+                            .fill(.white)
+                            .shadow(color: .gray.opacity(0.2), radius: 10, x: 0, y: 5)
                             .onTapGesture {
                                 focusedField = .none
                             }
                         
-                        VStack {
-                            HStack {
-                                Image(systemName: "person")
-                                    .foregroundColor(.gray)
-                                TextField("username", text: $username)
-                                    .focused($focusedField, equals: .usernameField)
-                                    .autocorrectionDisabled()
-                                    .textInputAutocapitalization(.never)
-                                    .onChange(of: focusedField) { newValue in
-                                        if focusedField == .usernameField {
-                                            loginCharacter
-                                                .setInput("isChecking", value: true)
-                                        } else {
-                                            loginCharacter
-                                                .setInput("isChecking", value: false)
-                                        }
-                                    }
-                                    .onChange(of: username.count) { newValue in
-                                        loginCharacter
-                                            .setInput("numLook", value: Float(newValue))
-                                    }
-                            }
-                            .padding()
-                            .background(.gray.opacity(0.1))
-                            .cornerRadius(5)
-                            
-                            HStack {
-                                Image(systemName: "lock")
-                                    .foregroundColor(.gray)
-                                SecureField("password", text: $password)
-                                    .focused($focusedField, equals: .passwordField)
-                                    .onChange(of: focusedField) { newValue in
-                                        if focusedField == .passwordField {
-                                            loginCharacter
-                                                .setInput("isHandsUp", value: true)
-                                        } else {
-                                            loginCharacter
-                                                .setInput("isHandsUp", value: false)
-                                        }
-                                    }
-                            }
-                            .padding()
-                            .background(.gray.opacity(0.1))
-                            .cornerRadius(5)
-                            
-                            Button {
-                                if !username.isEmpty && !password.isEmpty {
+                        VStack(spacing: 0) {
+                            Text("TEDDY BEAR")
+                                .foregroundColor(Color("fg"))
+                                .font(.system(size: 40))
+                                .fontWeight(signInSuccessful ?? false ? .bold:.medium)
+                                .contentTransition(.interpolate)
+                                .scaleEffect(signInSuccessful ?? false ? 1.12:1)
+                                .animation(.easeInOut, value: signInSuccessful)
+                                .tracking(10)
+                                .padding(.top)
+                                .offset(x: 5)
+                                .onTapGesture {
                                     focusedField = .none
-                                    isLoading = true
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.4) {
-                                        isLoading = false
-                                        if username == "iosdev22" && password == "seda22" {
-                                            signInSuccessful = true
+                                }
+                            
+                            VStack {
+                                HStack {
+                                    Image(systemName: "person")
+                                        .foregroundColor(.gray)
+                                    TextField("username", text: $username)
+                                        .focused($focusedField, equals: .usernameField)
+                                        .autocorrectionDisabled()
+                                        .textInputAutocapitalization(.never)
+                                        .onChange(of: focusedField) { newValue in
+                                            if focusedField == .usernameField {
+                                                loginCharacter
+                                                    .setInput("isChecking", value: true)
+                                            } else {
+                                                loginCharacter
+                                                    .setInput("isChecking", value: false)
+                                            }
+                                        }
+                                        .onChange(of: username.count) { newValue in
                                             loginCharacter
-                                                .triggerInput("trigSuccess")
-                                        } else {
-                                            signInSuccessful = false
-                                            loginCharacter
-                                                .triggerInput("trigFail")
+                                                .setInput("numLook", value: Float(newValue))
+                                        }
+                                }
+                                .padding()
+                                .background(.gray.opacity(0.1))
+                                .cornerRadius(5)
+                                
+                                HStack {
+                                    Image(systemName: "lock")
+                                        .foregroundColor(.gray)
+                                    SecureField("password", text: $password)
+                                        .focused($focusedField, equals: .passwordField)
+                                        .onChange(of: focusedField) { newValue in
+                                            if focusedField == .passwordField {
+                                                loginCharacter
+                                                    .setInput("isHandsUp", value: true)
+                                            } else {
+                                                loginCharacter
+                                                    .setInput("isHandsUp", value: false)
+                                            }
+                                        }
+                                }
+                                .padding()
+                                .background(.gray.opacity(0.1))
+                                .cornerRadius(5)
+                                
+                                Button {
+                                    if !username.isEmpty && !password.isEmpty {
+                                        focusedField = .none
+                                        isLoading = true
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.4) {
+                                            isLoading = false
+                                            if username == "iosdev22" && password == "seda22" {
+                                                signInSuccessful = true
+                                                loginCharacter
+                                                    .triggerInput("trigSuccess")
+                                                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                                                    withAnimation(.easeInOut) {
+                                                        self.showOnboarding = false
+                                                    }
+                                                }
+                                            } else {
+                                                signInSuccessful = false
+                                                loginCharacter
+                                                    .triggerInput("trigFail")
+                                            }
                                         }
                                     }
-                                }
+                                        
                                     
-                                
-                            } label: {
-                                Text("Sign In")
-                                    .fontWeight(.medium)
-                                    .foregroundColor(.white)
-                                    .frame(maxWidth: .infinity)
-                                    .padding(.vertical)
-                                    .background(.black.opacity(0.75))
-                                    .cornerRadius(5)
+                                } label: {
+                                    Text("Sign In")
+                                        .fontWeight(.medium)
+                                        .foregroundColor(.white)
+                                        .frame(maxWidth: .infinity)
+                                        .padding(.vertical)
+                                        .background(.black.opacity(0.75))
+                                        .cornerRadius(5)
+                                }
                             }
-                        }
-                        .padding(.top)
+                            .padding(.top)
 
-                        
-                        Spacer()
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                            
                     }
+                    .frame(height: 300)
                     .padding(.horizontal)
                         
-                }
-                .frame(height: 300)
-                .padding(.horizontal)
+                    Spacer()
                     
-                Spacer()
-                
-                Text("Already have an account? **Sign In**")
-                    .font(.system(size: 15))
-                    .foregroundColor(Color("fg"))
+                    if signInSuccessful != true {
+                        Text("Already have an account? **Sign In**")
+                            .font(.system(size: 15))
+                            .foregroundColor(Color("fg"))
+                            .transition(.opacity.animation(.easeInOut))
+                    }
+                }
+                .transition(AnyTransition.move(edge: .top).animation(.easeInOut))
+                .zIndex(1)
+            } else {
+                VStack {
+                    Spacer()
+                    Text("Welcome to my app")
+                        .font(.largeTitle)
+                        .bold()
+                    Text("Berkay Dişli")
+                        .font(.title2)
+                    Spacer()
+                }
+                .transition(AnyTransition.move(edge: .bottom).animation(.easeInOut))
+                .zIndex(1)
             }
             
             if isLoading {
-                RoundedRectangle(cornerRadius: 20)
-                    .fill(.ultraThinMaterial)
-                    .frame(width: 240, height: 240)
-                    .overlay {
-                        loadingView.view()
-                            .frame(width: 200, height: 200)
-                            .onAppear {
-                                loadingView.play()
-                            }
-                            .onDisappear {
-                                loadingView.stop()
-                            }
+                loadingView.view()
+                    .frame(width: 200, height: 200)
+                    .onAppear {
+                        loadingView.play()
+                    }
+                    .onDisappear {
+                        loadingView.stop()
                     }
                     .transition(AnyTransition.opacity.animation(.easeInOut))
+                    .zIndex(2)
             }
         }
     }
